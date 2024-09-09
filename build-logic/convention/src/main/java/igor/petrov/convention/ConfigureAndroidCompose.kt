68341180a38170c570e3a8638076a7ced753c034
Igor <igor.petrov.krsk@gmail.com>
2024-09-09 14:03:50 -0600

    Kotlin 2.0 update

--- build-logic/convention/src/main/java/igor/petrov/convention/ConfigureAndroidCompose.kt
+++ build-logic/convention/src/main/java/igor/petrov/convention/ConfigureAndroidCompose.kt
@@ -10,9 +10,6 @@ internal fun Project.configureAndroidCompose(
         buildFeatures {
             compose = true
         }
-        composeOptions{
-            kotlinCompilerExtensionVersion = libs.findVersion("composeCompiler").get().toString()
-        }
 
         dependencies {
             val bom = libs.findLibrary("androidx.compose.bom").get()
